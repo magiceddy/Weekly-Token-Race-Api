3ddae17a18a506be2ad8e7c6ed33cf3a54e839a8
magiceddy <rinaldo.rossi.web@gmail.com>
2018-08-27 20:20:20 +0200

    add exios mock

---
+++ src/__mocks__/data.js
@@ -0,0 +1,37 @@
+export const addressInfo = {
+  address: '0x86f7bb79564de79fff78ec2795cf58bed7034407',
+  ETH: {
+    balance: 8.95589125963
+  },
+  tokens: [
+    {
+      'tokenInfo': {
+        'address': '0x0abdace70d3790235af448c88547603b945604ea',
+        'name': 'district0x Network Token',
+        'decimals': '18',
+        'symbol': 'DNT',
+        'totalSupply': '1000000000000000000000000000',
+        'owner': '0x',
+        'lastUpdated': 1535375092,
+        'issuancesCount': 0,
+        'holdersCount': 30133,
+        'ethTransfersCount': 30,
+        'price': {
+          'rate': '0.0239741806',
+          'diff': 9.17,
+          'diff7d': 7.16,
+          'ts': '1535376267',
+          'marketCapUsd': '14384508.0',
+          'availableSupply': '600000000.0',
+          'volume24h': '502308.872927',
+          'diff30d': -35.837868058343,
+          'currency': 'USD'
+        }
+      },
+      'balance': 6.9493557619229e+22,
+      'totalIn': 0,
+      'totalOut': 0
+    }
+  ],
+  countTxs: 79
+}
